f54132c142cc70772ecc6e24cb1c01d56635c3be
diqoadnani02 <86178584+diqoadnani02@users.noreply.github.com>
2021-11-27 20:33:52 +0700

    restart

---
+++ src/Routes/Routes.js
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+export default function Routers() {
+  return (
+      <>
+      <Routes>
+          <Route />
+      </Routes>
+      </>
+  )
+}
