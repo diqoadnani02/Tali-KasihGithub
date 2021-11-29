451b5e3394487c0c12591453a9e582c44dcf023e
ardhyleo <adr_leo94@yahoo.com>
2021-11-29 10:58:01 +0700

    fixing: Home part3

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import Home from '../Pages/Home'
+import Home from "../Pages/Home";
 
 export default function Routers() {
   return (
-      <>
+    <>
       <Routes>
-          <Route exact path='/' element={<Home />} />
+        <Route exact path="/" element={<Home />} />
       </Routes>
-      </>
-  )
+    </>
+  );
+}
