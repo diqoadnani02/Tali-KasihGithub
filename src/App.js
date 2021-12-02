15ef8f8dc29887136586bb4a8cd6227f6ef52b14
diqoadnani02 <86178584+diqoadnani02@users.noreply.github.com>
2021-12-02 17:13:00 +0700

    take 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-// import Header from "./Components/Header/Header";
-// import Routers from "./Routes/Routes";
-// import Footer from './Components/Footer/Footer';
+import Header from "./Components/Header/Header";
+import Routers from "./Routes/Routes";
+import Footer from './Components/Footer/Footer';
 import "./App.css";
 import ProfileCard from './Components/ProfileCard/ProfileCard'
 
 function App() {
   return (
     <>
-      {/* <Header />
+      <Header />
       <Routers />
-      <Footer /> */}
-      <ProfileCard />
+      <Footer />
+      {/* <ProfileCard /> */}
     </>
   );
 }
