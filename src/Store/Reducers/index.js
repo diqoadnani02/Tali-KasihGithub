87326521157bba87b7ca7fb17bea8c83206151e0
diqoadnani02 <86178584+diqoadnani02@users.noreply.github.com>
2021-12-13 22:20:59 +0700

    add: my donation redux

--- src/Store/Reducers/index.js
+++ src/Store/Reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
 import authReducer from './authReducer/authReducer'
 import profileReducer from "./profile";
+import mydonationReducer from './mydonationReducer/mydonationReducer'
 
-const rootReducer = combineReducers({ auth: authReducer, profileReducer });
+const rootReducer = combineReducers({ auth: authReducer, profileReducer, mydonationReducer });
 
 
 export default rootReducer;
--- src/Store/Sagas/index.js
+++ src/Store/Sagas/index.js
@@ -2,10 +2,10 @@ import { all, call } from "redux-saga/effects";
 import { authSagas } from './authSagas/authSagas';
 import { watchProfile } from './profile';
 import {watchUpdateProfile} from './updateProfile';
-
+import {mydonationSagas} from './mydonationSagas/mydonationSagas'
 
 function* rootSaga() {
-  yield all([call(authSagas), watchProfile(), watchUpdateProfile()]);
+  yield all([call(authSagas), watchProfile(), watchUpdateProfile(), mydonationSagas()]);
 }
 
 export default rootSaga;
