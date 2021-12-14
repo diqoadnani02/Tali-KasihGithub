e7fe2b7cbcf0d6fec848cb205954a2f4ff1656bd
diqoadnani02 <86178584+diqoadnani02@users.noreply.github.com>
2021-12-14 17:18:23 +0700

    i'm working in here

--- src/Store/Reducers/index.js
+++ src/Store/Reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import authReducer from './authReducer/authReducer'
 import profileReducer from "./profile";
 import mydonationReducer from './mydonationReducer/mydonationReducer'
-import mycampaignReducer from './mycampaignReducer/mycampaingReducer'
+import mycampaignReducer from './mycampaignReducer/mycampaignReducer'
 
 const rootReducer = combineReducers({ auth: authReducer, profileReducer, mydonationReducer,campaign: mycampaignReducer });
 
