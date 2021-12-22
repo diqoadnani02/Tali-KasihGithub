504b52ced09cabcf8202a56bc3faefe02f653ffa
diqoadnani02 <86178584+diqoadnani02@users.noreply.github.com>
2021-12-22 17:18:00 +0700

    unit-testing home part 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home from './Pages/Home'
 
 
 
 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/new/i);
+test('render one pages', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/charity/i);
   expect(linkElement).toBeInTheDocument();
 });
