3f657a9a632a34ca7f348f9074da20059944c359
diqoadnani02 <86178584+diqoadnani02@users.noreply.github.com>
2021-12-22 20:39:02 +0700

    semoga bisa (lagi)

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Footer from "./Components/Footer/Footer";
 import "./App.css";
 import { useState } from "react";
 import SearchDiscover from "./Pages/Discover/SearchDiscover";
+import { Provider } from "react-redux";
+import store from "./Store";
+
 function App() {
   const [inputSearch, setInputSearch] = useState("");
   return (
     <>
+      <Provider store={store}>
       <Header inputSearch={inputSearch} setInputSearch={setInputSearch} />
       {inputSearch === "" ? <Routers inputSearch={inputSearch} /> : <SearchDiscover inputSearch={inputSearch} />}
       <Footer />
+      </Provider>
     </>
   );
 }
